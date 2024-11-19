6ec9dce83e6e5bdb05c947bb252c7dc0e7bcbc63
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-11-19 11:03:02 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Katelyn Curtiss
+# 19 November 2024
+# Return Statement
+
+# Project  1
+def describe_vacation(Greenland, Nuuk, Winter):
+   
+   return  f"I would love to visit, {Greenland} the city of {Nuuk} in the {Winter}!"
+
+print(return)
